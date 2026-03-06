299cc5d43268eb5002a88d4942de11c4f525ab53
MU-ty <3417633465@qq.com>
2026-03-06 20:21:46 +0800

    chore: 更新远程仓库地址并同步 vite 配置

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/guilin-citywalk/', // 替换为你的仓库名称
+  base: '/guilin-travel-and-trae/', // 更新为新的仓库名称
 })
